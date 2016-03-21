154d76b345512eda62c44d1d3c3516ca306e4801
Paco Gomez <fjgomez@florida-uni.es>
2016-03-21 19:41:56 +0100

    actualizando

---
+++ ProySalud/src/Interfaz/Principal.java
@@ -0,0 +1,14 @@
+package Interfaz;
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
